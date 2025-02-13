72c56952a5e7ab5364e9c35b8f1cc06eccb5a2d0
Janak kr Shrestha <janakkrshrestha@gmail.com>
2025-02-13 19:20:38 +0545

    Add comments to Input component for improved clarity and understanding

--- src/Components/Inputs.js
+++ src/Components/Inputs.js
@@ -1,10 +1,15 @@
 // Input.js
-import React from "react";
+import React from "react"; // Import the React library, which is essential for creating React components
 
+// Define a React component named 'Input' by extending React.Component
 class Input extends React.Component {
+  // The render method is called when the component is rendered. It returns the JSX that is used to render the component.
   render() {
     return (
       <div>
+        {/* // Render an input element with the type 'text' and placeholder 'Search from location...'.
+        // The value of the input element is set to the location prop passed in from the parent component.
+        // The onChange event handler is set to the onChangeLocation prop passed in from the parent component. */}
         <input
           type="text"
           placeholder="Search from location..."
@@ -16,4 +21,5 @@ class Input extends React.Component {
   }
 }
 
+// Export the Input component as the default export
 export default Input;
